117a4b181dcf7fbaa213ad989bfcafa4b605da92
Ryunosuke Kidokoro <ryunosuke.kidokoro@invinc2020.onmicrosoft.com>
2025-04-16 14:49:46 +0900

    Update: enlarge navbar height and text size (PC view)

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function Navigation() {
   return (
     <header className="fixed top-0 left-0 w-full z-50 flex items-center bg-white">
-      {/* ロゴエリア */}
+      {/* ロゴ */}
       <div className="bg-white px-4 py-2">
         <Image
           src="/images/bsc-logo.jpg"
@@ -17,20 +17,28 @@ export default function Navigation() {
         />
       </div>
 
-      {/* ナビゲーション */}
+      {/* ナビゲーションメニュー */}
       <nav className="flex-1">
-        <ul className="flex justify-end flex-wrap sm:flex-nowrap text-white text-sm sm:text-base font-medium">
-          <li className="bg-[#c6a664] px-4 py-5 border-l border-white whitespace-nowrap">
-            <a href="#about" className="block text-center hover:underline">会社情報</a>
+        <ul className="flex justify-end flex-wrap sm:flex-nowrap text-white text-sm sm:text-[1.25rem] font-medium w-full">
+          <li className="bg-[#c6a664] px-4 py-6 border-l border-white whitespace-nowrap">
+            <Link href="#about" className="block text-center hover:underline">
+              会社情報
+            </Link>
           </li>
-          <li className="bg-[#c6a664] px-4 py-5 border-l border-white whitespace-nowrap">
-            <a href="#programs" className="block text-center hover:underline">留学プログラム</a>
+          <li className="bg-[#c6a664] px-4 py-6 border-l border-white whitespace-nowrap">
+            <Link href="#programs" className="block text-center hover:underline">
+              留学プログラム
+            </Link>
           </li>
-          <li className="bg-[#c6a664] px-4 py-5 border-l border-white whitespace-nowrap">
-            <a href="#support" className="block text-center hover:underline">BSCのサポート</a>
+          <li className="bg-[#c6a664] px-4 py-6 border-l border-white whitespace-nowrap">
+            <Link href="#support" className="block text-center hover:underline">
+              BSCのサポート
+            </Link>
           </li>
-          <li className="bg-[#c6a664] px-4 py-5 border-l border-white whitespace-nowrap">
-            <a href="#contact" className="block text-center hover:underline">お問い合わせ</a>
+          <li className="bg-[#c6a664] px-4 py-6 border-l border-white whitespace-nowrap">
+            <Link href="#contact" className="block text-center hover:underline">
+              お問い合わせ
+            </Link>
           </li>
         </ul>
       </nav>
