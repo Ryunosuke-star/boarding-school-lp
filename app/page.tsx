652c085bccccc8b874fba252eeb67ebd975b1052
Ryunosuke Kidokoro <ryunosuke.kidokoro@invinc2020.onmicrosoft.com>
2025-04-16 14:09:46 +0900

    Add Navigation to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 
+import Navigation from "@/components/navigation"
 import { AboutSection } from "@/components/about-section"
 import ContactSection from "@/components/contact-section"
 import Footer from "@/components/footer"
 import HeroSection from "@/components/hero-section"
-import ProgramsSection from "@/components/programs-section" // ✅ 修正：named → default import
-import  SupportSection  from "@/components/support-section"
+import ProgramsSection from "@/components/programs-section"
+import SupportSection from "@/components/support-section"
 
 export default function Home() {
   return (
     <>
+      <Navigation />
       <HeroSection />
       <AboutSection />
       <ProgramsSection />
